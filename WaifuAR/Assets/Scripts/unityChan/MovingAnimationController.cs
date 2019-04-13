5c7dff6c03cefac78e8c81142984030848d6afdf
newplayerAR <shens495@hotmail.com>
2019-04-13 18:14:22 -0400

    -added unityChan idle animations

---
+++ WaifuAR/Assets/Scripts/unityChan/MovingAnimationController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+namespace UnityChan
+{
+    // Require these components when using this script
+    [RequireComponent(typeof(Animator))]
+
+    public class MovingAnimationController : MonoBehaviour
+    {
+        Animator animator;
+        NavMeshAgent agent;
+
+        void Awake()
+        {
+            animator = GetComponent<Animator>();
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            animator.SetFloat("Speed", agent.velocity.magnitude);
+        }
+    }
+}
